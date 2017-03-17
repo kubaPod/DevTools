4d065393f5970739fa87b79e7dccbbdc1f6a5ce6
Kuba Podkalicki <kuba.pod@gmail.com>
2017-03-17 23:17:35 +0100

    init

---
+++ PacletInfo.m
@@ -0,0 +1,20 @@
+(* ::Package:: *)
+
+
+Paclet[
+    Name -> "MoreStyles",
+    Version -> "0.1.0",
+    MathematicaVersion -> "10+",
+    Description -> "MoreStyles",
+    Creator -> "
+		Jakub (Kuba) Podkalicki
+		kuba.pod@gmail.com
+	",
+    Extensions -> 
+        {
+            {"Kernel", Context -> "MoreStyles`"},
+            {"FrontEnd"}
+        }
+]
+
+
