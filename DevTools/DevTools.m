08f2405ca0976b8a5413b9aa28eb0f673927a0d6
Kuba Podkalicki <kuba.pod@gmail.com>
2018-01-21 01:20:41 +0100

    fix(dev) single package setup

--- DevTools/DevTools.m
+++ DevTools/DevTools.m
@@ -2,16 +2,55 @@
 (* :Date: 2017-04-23 *)
 
 
-BeginPackage["DevTools`"]
-(* Exported symbols added here with SymbolName::usage *)
-
-  Get @ "DevTools`Kernel`Events`";
+BeginPackage["DevTools`"];
 
 
-  Begin["`Private`"];
+BeginPackage["`Kernel`"];
+
+IndentCode;
+
+EndPackage[];
 
 
-  End[];
+Begin["`Events`"];
+
+IndentCode // ClearAll;
+
+IndentCode[tab_String:"  "]:= With[
+  { sel = CurrentValue["SelectionData"]
+
+  }
+  , Module[
+    { selString := First @ FrontEndExecute[FrontEnd`ExportPacket[BoxData@sel, "PlainText"]]
+      , steps (*counted because we need to move selection back where it was before action //modulo indentation*)
+      , result
+    }
+    , Which[
+      sel === $Failed
+      , result = tab
+      ; steps = 0
+
+      , StringQ[sel]
+      , result = tab <> sel
+      ; steps = StringLength[result]
+
+      , True
+      , result = StringReplace[ StringDrop[selString, -1], {"\r\n" :> ("\n" <> tab),"\r"->""} ]
+      ; result = tab <> result <> StringTake[selString,-1]
+      ; steps = StringLength @ result
+    ]
+
+    ; If[
+      steps == 0
+      , NotebookWrite[EvaluationNotebook[], BoxData @ result, After]
+      , NotebookWrite[EvaluationNotebook[], BoxData @ result, Before]
+      ; SelectionMove[EvaluationNotebook[], All, Character, steps]
+    ]
+  ]
+];
+
+
+End[]
 
 
 EndPackage[]
