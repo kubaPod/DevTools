ac5a2160e79bd23eddbb9236614131f5c579b419
Kuba <kuba.pod@gmail.com>
2018-11-30 10:59:06 +0100

    fix(actions) SelectCell handle no cell

--- DevTools/Resources/NotebookActions.m
+++ DevTools/Resources/NotebookActions.m
@@ -1,5 +1,5 @@
 <|
   "Label" -> "Select cell"
 , "ShortKey" -> "a"
-, "Action" :> SelectionMove[First @ SelectedCells @ EvaluationNotebook[],All,Cell]
+, "Action" :> (If[Length[#] > 0 , SelectionMove[First @ # ,All,Cell]]& @ SelectedCells @ EvaluationNotebook[])
 |>
