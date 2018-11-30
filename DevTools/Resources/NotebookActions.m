e367550f808d45b0bc0c0a80f395c8fb90f2d764
Kuba <kuba.pod@gmail.com>
2018-11-30 11:07:23 +0100

    update(actions) key directories shortcuts

--- DevTools/Resources/NotebookActions.m
+++ DevTools/Resources/NotebookActions.m
@@ -3,3 +3,21 @@
 , "ShortKey" -> "a"
 , "Action" :> (If[Length[#] > 0 , SelectionMove[First @ # ,All,Cell]]& @ SelectedCells @ EvaluationNotebook[])
 |>
+
+<|
+  "Label" -> "$InstallationDir"
+  
+  , "Action" :> SystemOpen @ $InstallationDirectory
+|>
+
+<|
+  "Label" -> "$UserBaseDir"
+  
+  , "Action" :> SystemOpen @ $InstallationDirectory
+|>
+
+<|
+  "Label" -> "NotebookDirectory"
+  
+  , "Action" :> SystemOpen @ $InstallationDirectory
+|>
