44b215ca6e8a0df386a84b6eba9e2c86e06487ca
Kuba Podkalicki <kuba.pod@gmail.com>
2019-11-28 15:05:26 +0100

    0.10.5

--- DevTools/PacletInfo.m
+++ DevTools/PacletInfo.m
@@ -1,5 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "DevTools", Version -> "0.10.4", WolframVersion -> "10.4+", 
+Paclet[Name -> "DevTools", Version -> "0.10.5", WolframVersion -> "10.4+", 
  Description -> "DevTools", 
  Creator -> "Jakub (Kuba) Podkalicki\tkuba.pod@gmail.com", 
  Extensions -> {{"FrontEnd"}, {"Kernel", Context -> "DevTools`"}}]
